7d112628de2ea73b7281fef8110de9134730fd7c
BerkeBerktay <bberktay19@ku.edu.tr>
2021-11-25 00:05:50 +0300

    last commit before output files

--- game_of_life.c
+++ game_of_life.c
@@ -5,15 +5,44 @@
 int iterations;
 int rows;
 int columns;
+int** currentiteration;
+
+// print the game state
+// after each step you can call this function, add some delay and then clear screen to create a simple game simulation in terminal
+void print_state(int **state)
+{
+    int i, j;
+    printf("\nNext State:\n");
+    for(i=0; i<rows; i++){
+        for(j=0; j<columns; j++){
+            printf("%d", state[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// print the game state
+// after each step you can call this function, add some delay and then clear screen to create a simple game simulation in terminal
+void print_statechar(char **state)
+{
+    int i, j;
+    printf("\nNext State:\n");
+    for(i=0; i<rows; i++){
+        for(j=0; j<columns; j++){
+            printf("%c", state[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 // read the input from given filename and return a 2D dynamically allocated and populated array
 // you can use char type for each cell value or bool from <stdbool.h>
-int **read_from_file(const char *filename)
+void read_from_file(const char *filename)
 {
     FILE *x;
     int count;
     count = 0;
     int i;
-    int** currentiteration;
     i = -1;
     x = fopen(filename,"r");//opens the file
     if (x != NULL)
@@ -58,13 +87,12 @@ int **read_from_file(const char *filename)
             }
         }
     }
-    print_state(currentiteration);
-    return currentiteration;
+    //print_state(currentiteration);
     fclose(x);
 }
 
 // given a game step return the next step applying game rules
-int **next_state(int **state)
+void next_state()
 {
 
     int** nextstate;
@@ -75,6 +103,7 @@ int **next_state(int **state)
     }//sets up the nextstate in the heap
     int i;
     int j;
+
     int** aliveneighbors;
     int l;
     aliveneighbors = (int**)malloc(rows * sizeof(int*));
@@ -86,31 +115,48 @@ int **next_state(int **state)
             aliveneighbors[i][j] = 0;//initializes the aliveneighbors since we will increment its values inside the 2d array
         }
     } 
+
+    char** outputmatrix;//char matrix for converting int cells to actual char outputs
+    outputmatrix = (char**)malloc(rows * sizeof(char*));
+    for (l = 0; l < rows; l++){
+        outputmatrix[l] = (char*) malloc(columns * sizeof(char));
+    }
+    for (i = 0; i < rows; i++){
+        for (j = 0; j < columns; j++){
+            if(currentiteration[i][j] == 0){
+                outputmatrix[i][j] = '-';
+            }
+            else{
+                outputmatrix[i][j] = 'X';
+            }
+        }
+    } 
+
     for (i = 0; i < rows; i++){//these for statements allow us to fill the alive neighbors matrix
        for (j = 0; j < columns; j++){
            if( i > 0 && j > 0){ //makes sure top left neighbor exists
-               aliveneighbors[i][j] = aliveneighbors[i][j] + state[i-1][j-1];
+               aliveneighbors[i][j] = aliveneighbors[i][j] + currentiteration[i-1][j-1];
            }
            if( i>0 && j< columns - 1){//makes sure top right neighbor exists
-               aliveneighbors[i][j] += state[i-1][j+1];
+               aliveneighbors[i][j] += currentiteration[i-1][j+1];
            }
            if( i>0){//makes sure top neighbor exists
-               aliveneighbors[i][j] += state[i-1][j];
+               aliveneighbors[i][j] += currentiteration[i-1][j];
            }
            if( i< rows - 1 && j>0){ //makes sure bottom left neighbor exists
-               aliveneighbors[i][j] += state[i+1][j-1];
+               aliveneighbors[i][j] += currentiteration[i+1][j-1];
            }
            if( i < rows-1 && j< columns - 1){//makes sure bottom right neighbor exists
-               aliveneighbors[i][j] += state[i+1][j+1];
+               aliveneighbors[i][j] += currentiteration[i+1][j+1];
            }
            if( i < rows - 1){//makes sure bottom neighbor exists
-               aliveneighbors[i][j] += state[i+1][j];
+               aliveneighbors[i][j] += currentiteration[i+1][j];
            }
            if(j>0){//makes sure left neighbor exists
-               aliveneighbors[i][j] += state[i][j-1];
+               aliveneighbors[i][j] += currentiteration[i][j-1];
            }
            if(j < columns - 1){//makes sure right neighbor exists
-               aliveneighbors[i][j] += state[i][j+1];
+               aliveneighbors[i][j] += currentiteration[i][j+1];
            }
         }
     } 
@@ -118,9 +164,11 @@ int **next_state(int **state)
     int f;
     int livecount;
     int deadcount;
+    deadcount = 0;
+    livecount = 0;
     for (r = 0; r < rows; r++){//these for statements allow us to fill the next iteration matrix
        for (f = 0; f < columns; f++){
-           if(state[r][f] == 0){
+           if(currentiteration[r][f] == 0){
                if(aliveneighbors[r][f] == 3){
                    nextstate[r][f] = 1;//if the number of neighbors is 3, it will come alive
                    livecount++;
@@ -140,10 +188,17 @@ int **next_state(int **state)
            }
        }
     }    
-    printf("%d", deadcount);
-    printf("%d", livecount);
-    print_state(nextstate);
-    return nextstate;  
+    printf("%d\n", deadcount);
+    printf("%d\n", livecount);
+    for (r = 0; r < rows; r++){//these for statements allow us to transform the currentiteration matrix to our actual current iteration from the previous iteration 
+                                //by using the nextstate as a type of "dummy state"
+       for (f = 0; f < columns; f++){
+           currentiteration[r][f] = nextstate[r][f];
+       }
+    }    
+    //print_state(currentiteration);
+    //print_statechar(outputmatrix);
+
 }
 
 // bonus task
@@ -153,32 +208,16 @@ bool is_oscillator(char **pattern, int max_steps)
     return NULL;
 }
 
-// print the game state
-// after each step you can call this function, add some delay and then clear screen to create a simple game simulation in terminal
-void print_state(int **state)
-{
-
-    int i, j;
-    printf("\nNext State:\n");
-    for(i=0; i<rows; i++){
-        for(j=0; j<columns; j++){
-            printf("%d", state[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 int main(int argc, char *argv[]) 
 {
    const char *input;
    input = argv[1];
    const char *output;
    output = argv[2];//gets the inputs of the input path and the output path
-   nextstate(nextstate(nextstate(nextstate(nextstate(read_from_file(input))))));
+   read_from_file(input);
    int w;
-
-   /*for(w = 0; w < iterations; w++){
-
-   }*/
+   for(w = 0; w < iterations; w++){
+       next_state();
+   }
    return 0;
 }               
