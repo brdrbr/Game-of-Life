7edda8689e42237a77cfd0e9662262378792a7c3
BerkeBerktay <bberktay19@ku.edu.tr>
2021-11-24 06:44:43 +0300

    nextstate finction commit

--- game_of_life.c
+++ game_of_life.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
+int iterations;
+int rows;
+int columns;
 // read the input from given filename and return a 2D dynamically allocated and populated array
 // you can use char type for each cell value or bool from <stdbool.h>
 int **read_from_file(const char *filename)
@@ -10,9 +12,6 @@ int **read_from_file(const char *filename)
     FILE *x;
     int count;
     count = 0;
-    int iterations;
-    int rows;
-    int columns;
     int i;
     int** currentiteration;
     i = -1;
@@ -49,9 +48,11 @@ int **read_from_file(const char *filename)
                     if(line[j] == '-'){//checks each character in the line and assigns either 1 or 0 to the corresponding location in the heap
                     //depending on the line's character either being - or X
                         currentiteration[i][j] = 0;
+                        //printf("%d", currentiteration[i][j]);
                     }
                     else{
                         currentiteration[i][j] = 1;
+                        //printf("%d", currentiteration[i][j]);
                     }
                 }
             }
@@ -62,9 +63,79 @@ int **read_from_file(const char *filename)
 }
 
 // given a game step return the next step applying game rules
-char **next_state(char **state)
+int **next_state(int **state)
 {
-   return NULL;
+
+    int** nextstate;
+    int k;
+    nextstate = (int**)malloc(rows * sizeof(int*));
+    for (k = 0; k < rows; k++){
+        nextstate[k] = (int*) malloc(columns * sizeof(int));
+    }//sets up the nextstate in the heap
+    int i;
+    int j;
+    int** aliveneighbors;
+    int l;
+    aliveneighbors = (int**)malloc(rows * sizeof(int*));
+    for (l = 0; l < rows; l++){
+        aliveneighbors[l] = (int*) malloc(columns * sizeof(int));
+    }//we need another matrix to fill before we fill the nextstate matrix, this matrix will hold the number of alive neighbors for each cell
+    for (i = 0; i < rows; i++){
+        for (j = 0; j < columns; j++){
+            aliveneighbors[i][j] = 0;//initializes the aliveneighbors since we will increment its values inside the 2d array
+        }
+    } 
+    for (i = 0; i < rows; i++){//these for statements allow us to fill the alive neighbors matrix
+       for (j = 0; j < columns; j++){
+           if( i > 0 && j > 0){ //makes sure top left neighbor exists
+               aliveneighbors[i][j] = aliveneighbors[i][j] + state[i-1][j-1];
+           }
+           if( i>0 && j< columns - 1){//makes sure top right neighbor exists
+               aliveneighbors[i][j] += state[i-1][j+1];
+           }
+           if( i>0){//makes sure top neighbor exists
+               aliveneighbors[i][j] += state[i-1][j];
+           }
+           if( i< rows - 1 && j>0){ //makes sure bottom left neighbor exists
+               aliveneighbors[i][j] += state[i+1][j-1];
+           }
+           if( i < rows-1 && j< columns - 1){//makes sure bottom right neighbor exists
+               aliveneighbors[i][j] += state[i+1][j+1];
+           }
+           if( i < rows - 1){//makes sure bottom neighbor exists
+               aliveneighbors[i][j] += state[i+1][j];
+           }
+           if(j>0){//makes sure left neighbor exists
+               aliveneighbors[i][j] += state[i][j-1];
+           }
+           if(j < columns - 1){//makes sure right neighbor exists
+               aliveneighbors[i][j] += state[i][j+1];
+           }
+        }
+    } 
+    int r;
+    int f;
+    for (r = 0; r < rows; r++){//these for statements allow us to fill the next iteration matrix
+       for (f = 0; f < columns; f++){
+           if(state[r][f] == 0){
+               if(aliveneighbors[r][f] == 3){
+                   nextstate[r][f] = 1;//if the number of neighbors is 3, it will come alive
+               }
+               else{
+                   nextstate[r][f] = 0;//else it will stay dead
+               }
+           }
+           else{
+               if(aliveneighbors[r][f] == 2  || aliveneighbors[r][f] == 3){
+                   nextstate[r][f] = 1;//if the number of neighbors is 2 or 3 it will stay alive
+               }
+               else{
+                   nextstate[r][f] = 0;//else it will die
+               }
+           }
+       }
+    }     
+    return nextstate;  
 }
 
 // bonus task
@@ -96,6 +167,6 @@ int main(int argc, char *argv[])
    const char *output;
    output = argv[2];//gets the inputs of the input path and the output path
    //with open (input,"r") as f:
-   read_from_file(input);
+   next_state(read_from_file(input));
     return 0;
 }               
